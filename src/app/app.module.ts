3e1be464f09c4fc51e8f55010831f593f82861b4
Sudip Tamang <sudiptamang000@gmail.com>
2024-07-01 22:48:07 +0545

    fix: language translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
 }
 
 @NgModule({
--- src/app/core/home/home/home.component.ts
+++ src/app/core/home/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
+import { TranslateService } from '@ngx-translate/core';
 import { Observable } from 'rxjs';
 import { loadUsers } from 'src/app/store/actions/user.action';
 import { User } from 'src/app/store/models/user.model';
@@ -12,14 +13,25 @@ import { selectAllUser } from 'src/app/store/selectors/user.selector';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  currentLanguage:string;
   usersList$:Observable<User[]>;
-  constructor(private store:Store<{users:UserState}>) { 
+  constructor(private store:Store<{users:UserState}>,private translate: TranslateService) { 
     this.usersList$ = this.store.pipe(select(selectAllUser));
+    this.translate.use('en')
+    this.currentLanguage = this.translate.currentLang || this.translate.defaultLang;
+
+    this.translate.onLangChange.subscribe((evt:any)=>{
+      this.currentLanguage = evt?.lang;
+    })
   }
 
   ngOnInit(): void {
     this.store.dispatch(loadUsers());
+  }
+
+  changeLanguage():void{
+    const changeLang = this.currentLanguage==='en'?'np':'en';
+    this.translate.use(changeLang);
   }
 
 }
